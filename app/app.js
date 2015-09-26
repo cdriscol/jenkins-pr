6de84b5f3996421275f47d95b92cd63fece7fde4
cdriscol <drizkol@gmail.com>
2015-09-26 12:02:11 -0600

    adding coverage

---
+++ app/app.js
@@ -0,0 +1,8 @@
+(function() {
+  function someClass() {
+    function doesSomethingWith(a) {
+      if(a) console.log(a);
+      else console.error('what do you want me to do something with?')
+    }
+  }
+})();
